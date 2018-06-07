915a27f56815cbfef87c019f0f3f25e01ee2d126
Guillermo <you@example.com>
2018-06-07 15:23:49 +0200

    Edit code with TS

--- ej11- Factorial/ej11Factorial.ts
+++ ej11- Factorial/ej11Factorial.ts
@@ -1,6 +1,7 @@
 var resultado = 1;
 var numero = parseInt(prompt("Introduce el número a factorizar: "));
-for (var i = 1; i <= numero; i++) {
-    resultado *= i;
+
+for(var i=1; i<=numero; i++){
+        resultado *=  i;
 }
 alert(resultado);
